d48ee5113d3c4f543a27d1f67bc048edf0fa8e73
Michael Lavella <mel8978@g.rit.edu>
2025-03-17 13:04:10 -0400

    Initaial_PreConflict

---
+++ ConflictTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConflictTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
