67f665ae2f6949312f015299f1ee9883d86deab5
Kai Lee (RIT Student) <kjleesf@gmail.com>
2025-03-17 13:48:43 -0400

    Architecture_initialize

---
+++ Lethal_Organization/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lethal_Organization
+{
+
+    public enum GameState
+    {
+
+    }
+
+    internal class GameManager
+    {
+        private static GameManager _instance;
+        private static readonly object _lock = new object();
+
+        private GameManager() { }
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new GameManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Lethal_Organization/Obstacles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lethal_Organization
+{
+    internal class Obstacles
+    {
+    }
+}
