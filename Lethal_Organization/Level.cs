9025a3548613d5e1fa579ce27015a0bbd2330b11
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-03-19 15:55:17 -0500

    Initiliaze spriteSheet_txt for texture map
    Add Level.Cs, tile.cs to create level

---
+++ Lethal_Organization/Level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lethal_Organization;
+
+/// <summary>
+/// Collection of tile info
+/// Save to fileIO the layout of level
+/// 
+/// </summary>
+public class Level
+{
+    private SpriteBatch _spriteBatch; 
+        
+    private Texture2D _spriteSheet;
+    private Tile[,] _map;
+    private int _drawnSize; //Size of the real image drawn in window for each tile
+
+    private Dictionary<string, Rectangle> _textureMap;
+
+    public Level(Texture2D spriteSheet, string textureMapFile, SpriteBatch spriteBatch)
+    {
+        this._spriteSheet = spriteSheet;
+        this._spriteBatch = spriteBatch;
+        
+        //In sprite sheet, each tile is 15x16 but drawn as 32x32 in screen
+        _drawnSize = 32;
+        
+        _textureMap = new Dictionary<string, Rectangle>(); 
+        
+    }
+
+}
---
+++ Lethal_Organization/Tile.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lethal_Organization;
+
+public class Tile
+{
+   private Rectangle _posRect;
+   private Rectangle _sourceRect;
+   private Texture2D _spriteSheet;
+       
+    /// <summary>
+    /// Constructs a LevelTile object
+    /// </summary>
+    /// <param name="posRect">Where will this tile be drawn in the game window?</param>
+    /// <param name="rowIndex">Which row is the source tile in the sprite sheet?</param>
+    /// <param name="colIndex">Which column is the source tile in the sprite sheet?</param>
+    /// <param name = "width" > Pixel width of the tile
+    /// <param name = "height" > Pixel height of the tile
+    public Tile(Rectangle posRect, Texture2D spriteSheet, int rowIndex, int colIndex, int width, int height)
+   {
+       this._spriteSheet = spriteSheet;
+       this._posRect = posRect;
+       this._sourceRect = CalculateSourceRect(spriteSheet, rowIndex, colIndex, width, height);
+   }
+
+    
+    /// <summary>
+    /// STANDARD PIXEL OF COLUMNS AND ROWS: 16X16
+    /// CalculateSourceRect base on row index, col index, pixel rate
+    /// No space between each tile
+    /// </summary>
+    /// <param name="spriteSheet"></param>
+    /// <param name="rowIndex"></param>
+    /// <param name="colIndex"></param>
+    /// <returns></returns>
+    private Rectangle CalculateSourceRect(Texture2D spriteSheet, int rowIndex, int colIndex, int width, int height)
+    {
+        return new Rectangle
+        (rowIndex * 16   //Standard pixel in sprite sheet is 16x16
+        , colIndex * 16
+        , width, height);
+    }
+
+
+
+    /// <summary>
+    /// Draw tile
+    /// Expected to call sb.Begin()/End() elsewhere
+    /// </summary>
+    /// <param name="spriteBatch"></param>
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(
+            _spriteSheet,
+            _posRect,
+            _sourceRect,
+            Color.White,
+            0,
+            Vector2.Zero,
+            SpriteEffects.None,
+            0
+            );
+    }
+   
+
+}
