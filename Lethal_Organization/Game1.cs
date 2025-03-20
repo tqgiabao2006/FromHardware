88c1cb213d973afa054fbab1338164a3832a12ec
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-03-19 20:30:19 -0500

    Finish_Level_SPrite_Set_Up

--- Lethal_Organization/Game1.cs
+++ Lethal_Organization/Game1.cs
@@ -8,6 +8,9 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    //Level:
+    private Texture2D _tileSpriteSheet;
 
     public Game1()
     {
@@ -18,16 +21,17 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
+      
         base.Initialize();
+        
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        // TODO: use this.Content to load your game content here
+        //Test-only
+        _tileSpriteSheet = Content.Load<Texture2D>("TileSpriteSheet");
+        Level level = new Level(_tileSpriteSheet, "../../../Content/textureMap.txt", 1,1, _spriteBatch);
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,7 +39,6 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
 
         base.Update(gameTime);
     }
@@ -44,7 +47,6 @@ public class Game1 : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
 
         base.Draw(gameTime);
     }
--- Lethal_Organization/Level.cs
+++ Lethal_Organization/Level.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -15,20 +17,66 @@ public class Level
         
     private Texture2D _spriteSheet;
     private Tile[,] _map;
-    private int _drawnSize; //Size of the real image drawn in window for each tile
+    private int _drawHeightScale; //Scale of the real image drawn in window for each tile. If tile 16x32, scale 2 => 32x64
+    private int _drawWidthScale;
 
     private Dictionary<string, Rectangle> _textureMap;
 
-    public Level(Texture2D spriteSheet, string textureMapFile, SpriteBatch spriteBatch)
+    public Level(Texture2D spriteSheet, string textureMapFile, int drawnHeightScale, int drawWidthScale, SpriteBatch spriteBatch)
     {
         this._spriteSheet = spriteSheet;
         this._spriteBatch = spriteBatch;
         
-        //In sprite sheet, each tile is 15x16 but drawn as 32x32 in screen
-        _drawnSize = 32;
-        
-        _textureMap = new Dictionary<string, Rectangle>(); 
-        
-    }
+        _textureMap = new Dictionary<string, Rectangle>();
 
+        StreamReader reader = null;
+        try
+        {
+            reader = new StreamReader(textureMapFile);
+            string line = "";
+
+            int currentWidth = 0;
+            int currentHeight = 0;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                //Skip the line start by '/' or '=' because it is description, not data
+                if (line[0] == '/' || line[0] == '=')
+                {
+                    continue;
+                }
+
+                string[] data = line.Split(',');
+
+                //If it is size data
+                if (data.Length == 2)
+                {
+                    currentWidth = int.Parse(data[0]);
+                    currentHeight = int.Parse(data[1]);
+                }
+                else if (data.Length == 3)
+                {
+                    _textureMap.Add(
+                        data[0], //Tile Name
+                        new Rectangle(
+                            int.Parse(data[2]) * 16, //X-pivot = ColIndex * 16 (16 is standard pixel scale, no space between tile)
+                            int.Parse(data[1]) * 16, //Y-pivot = RowIndex * 16 
+                            int.Parse(data[2]) * 16 + currentWidth, //Width = X-pivot + current width
+                            int.Parse(data[1]) * 16 + currentWidth) //Height = Y-Pivot + current height
+                    );
+                }
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            Console.WriteLine("ERROR: Can not find the text file!");
+        }
+        finally
+        {
+            if (reader != null)
+            {
+                reader.Close();
+            }
+        }
+    }
 }
