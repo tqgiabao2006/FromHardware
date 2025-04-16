5ba48dc9f1d031f935475319a91d819559049d7c
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-04-15 23:48:08 -0500

    Finish Map Design

--- Lethal_Organization/Level.cs
+++ Lethal_Organization/Level.cs
@@ -254,7 +254,6 @@ public class Level: IStateChange
                             );
                         }
                     }
-                    
                     currentRow++;
                 }
                 
