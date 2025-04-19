3be4dfc2ae56075a0f93090a88171ca53c6c103d
tqgiabao2006 <138303770+tqgiabao2006@users.noreply.github.com>
2025-04-19 17:11:13 -0500

    Create HeatlhBar.cs

---
+++ Lethal_Organization/HeatlhBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Lethal_Organization
+{
+    class HeatlhBar: UI
+    {
+        private Texture2D _UISPriteSHeet;
+
+        private Rectangle _UIBar;
+
+        public bool Visible { get; set; }
+
+        public HeatlhBar()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+
+        }
+
+        public void Hide()
+        {
+
+        }
+
+        public void Show()
+        {
+
+        }
+
+        public void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+
+        }
+    }
+}
